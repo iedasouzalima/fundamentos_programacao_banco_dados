1fe3860f6add7f38678dd351939492628e35cf5e
iedasouzalima <147563330+iedasouzalima@users.noreply.github.com>
2024-07-03 21:39:45 -0300

    condicionais e laços de repetições
    aula do dia 03/07/2024

---
+++ ieda-Python/condicionais.py
@@ -0,0 +1,27 @@
+#print('Boa noite')
+
+#idade = 16
+#variavel condicional com nome idade
+#if idade == 16:
+#condicçao da variavel idade é 16
+    # print('menor de idade')
+     #resultado da variavel
+#FLOAT É DECIMAIS
+
+#year = int(input('Qual é a sua idade?'))
+#if year <= 16:
+#  print('Você é menor de idade')
+#else :
+#   print("Você é maior de idade")
+
+faixaEtaria = int(input('Qual é sua idade? '))
+if faixaEtaria >18:
+   print('filme de terror')
+#elif faixaEtaria <=18:
+   #print('filme de ação')
+elif faixaEtaria >=14:
+  print('filme de ação')
+#elif faixaEtaria :
+ #  print('filme infantil')
+else :
+   print('filme infantil')      
---
+++ ieda-Python/laçosderepetiçao.py
@@ -0,0 +1,14 @@
+##isso é uma lista
+#x = 0
+#while x < 5: 
+#quantidade de nomes(5)    
+#   nome = input ('Qual é o seu nome? ')
+#   x=x+1
+
+x = 0
+animal =[ ]
+while x < 5 :
+    nomeAnimal = input('Nomes de 5 animais com a letra I: ')
+    animal.append (animal)
+    x=x+1
+    print(animal)
